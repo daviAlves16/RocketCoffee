8b9fbf067e0c3c9680ea452fbc6d40b3a2ea2f53
Davi Alves <daviice2011@hotmail.com>
2022-07-03 19:12:36 -0300

    Nono Commit

--- arquivos/pagina.js
+++ arquivos/pagina.js
@@ -1,10 +1,18 @@
-jQuery(function () {
-    wit = screen.width
-    jQuery(window).resize(function () {
-    if (jQuery(this).width < 664){
-        $("#rock").removeClass("none");
-    }else{
-        $("#rock").addClass("none");
-    } 
-    });
-    });
+
+    function myFunction() {
+        var x = document.getElementById("myLinks");
+        var y = document.getElementById("logoAbrir")
+        var z = document.getElementById("logoFechar")
+        if (x.style.display === "block") {
+          x.style.display = "none";
+          
+          y.style.display = "block";
+          z.style.display = "none";
+          
+        } else {
+          x.style.display = "block";
+          
+          y.style.display = "none";
+          z.style.display = "block";
+        }
+      }
